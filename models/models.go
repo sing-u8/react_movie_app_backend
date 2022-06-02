40091c7d805509e1eb84d579cc16edc6a59971b4
sing-u8 <singyu13@gmail.com>
2022-06-02 22:54:08 +0900

    sec45. An aside: cleaning up our JSON feed

--- models/models.go
+++ models/models.go
@@ -19,17 +19,17 @@ func NewModels(db *sql.DB) Models {
 
 // Movie is the type for movies
 type Movie struct {
-	ID          int          `json:"id"`
-	Title       string       `json:"title"`
-	Description string       `json:"description"`
-	Year        int          `json:"year"`
-	ReleaseDate time.Time    `json:"release_date"`
-	Runtime     int          `json:"runtime"`
-	Rating      int          `json:"rating"`
-	MPAARating  string       `json:"mpaa_rating"`
-	CreatedAt   time.Time    `json:"-"`
-	UpdatedAt   time.Time    `json:"-"`
-	MovieGenre  []MovieGenre `json:"genres"`
+	ID          int            `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Year        int            `json:"year"`
+	ReleaseDate time.Time      `json:"release_date"`
+	Runtime     int            `json:"runtime"`
+	Rating      int            `json:"rating"`
+	MPAARating  string         `json:"mpaa_rating"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt   time.Time      `json:"-"`
+	MovieGenre  map[int]string `json:"genres"`
 }
 
 // Genre is the type for genre
@@ -49,21 +49,3 @@ type MovieGenre struct {
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
-
-// Genre is the type for genre
-// type Genre struct {
-// 	ID        int       `json:"id"`
-// 	GenreName string    `json:"genre_name"`
-// 	CreatedAt time.Time `json:"created_at"`
-// 	UpdatedAt time.Time `json:"updated_at"`
-// }
-
-// // MovieGenre is the type for movie genre
-// type MovieGenre struct {
-// 	ID        int       `json:"id"`
-// 	MovieID   int       `json:"movie_id"`
-// 	GenreID   int       `json:"genre_id"`
-// 	Genre     Genre     `json:"genre"`
-// 	CreatedAt time.Time `json:"created_at"`
-// 	UpdatedAt time.Time `json:"updated_at"`
-// }
